9fefce0349754b8595a425da0fbf88fb371d5448
DarthSeveros <joseivecas@gmail.com>
2023-03-27 22:22:37 -0300

    added redirect from home to solicitudes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('indices.index'));
 });
 
 Route::controller(SolicitudesController::class)->prefix('solicitud')->group(function() {
